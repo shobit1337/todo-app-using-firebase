d4aeca9f4c285948e7741494893414b1500d00ce
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-15 01:53:29 +0530

    fix: working again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,84 @@
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  Button,
+  Form,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+import { db } from "./firebaseConfig";
+import firebase from "firebase";
+
+import Todos from "./Components/Todos.js";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
+  };
+
+  function addTodo(e) {
+    e.preventDefault();
+    if (todoInput === "") {
+      // do nothing
+    } else {
+      db.collection("todos").add({
+        inprogress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: todoInput,
+      });
+    }
+    setTodoInput("");
+  }
+
+  return (
+    <Container fluid>
+      <h1> Todo with Firebase</h1>
+      <div>
+        <Form>
+          <FormGroup>
+            <InputGroup>
+              <Input
+                type="text"
+                name="todo"
+                id="todo"
+                placeholder="Enter a todo task"
+                value={todoInput}
+                onChange={(e) => setTodoInput(e.target.value)}
+              />
+              <InputGroupAddon addonType="prepend">
+                <Button color="success" onClick={addTodo}>
+                  Add Todo
+                </Button>
+              </InputGroupAddon>
+            </InputGroup>
+          </FormGroup>
+        </Form>
+      </div>
+      <div style={{ marginBottom: "2px" }}>
+        <Todos todo={todos} />
+      </div>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/Components/Todos.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { RiDeleteBin2Fill, RiTaskLine, RiTaskFill } from "react-icons/ri";
+import { db } from "../firebaseConfig";
+
+const Todos = ({ todo }) => {
+  //const [inprogress, setInprogress] = useState(false);
+  function toggleInProgress(id, inprogress) {
+    db.collection("todos").doc(id).update({
+      inprogress: !inprogress,
+    });
+  }
+
+  function deleteTodo(id) {
+    db.collection("todos").doc(id).delete();
+  }
+
+  return (
+    <ListGroup className="mt-5 mb-2 items">
+      {todo.map((todo) => (
+        <ListGroupItem key={todo.id}>
+          <span className={todo.inprogress ? "todo" : "todo done"}>
+            {todo.todo}
+          </span>
+          <span
+            className="float-left icon"
+            onClick={() => toggleInProgress(todo.id, todo.inprogress)}
+          >
+            {todo.inprogress ? <RiTaskLine /> : <RiTaskFill />}
+          </span>
+
+          <span
+            className="float-right icon"
+            onClick={() => deleteTodo(todo.id)}
+          >
+            <RiDeleteBin2Fill />
+          </span>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+};
+
+export default Todos;
